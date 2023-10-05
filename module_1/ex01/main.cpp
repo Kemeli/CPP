1db646749b95d6b6aa9b6f2851a5c0c7793ab934
Kemeli <kdnogueira791@gmail.com>
2023-10-05 11:58:08 -0300

    fixes add ctrl d issue

--- module_1/ex01/main.cpp
+++ module_1/ex01/main.cpp
@@ -9,30 +9,29 @@ void	add(PhoneBook &agenda)
 	std::string	phone_num;
 	std::string	dark_secret;
 
-	while (first_n.empty()){
+	while (first_n.empty() && !std::cin.eof()){
 		std::cout << "Enter firt name: ";
 		std::getline(std::cin, first_n);
 	}
-	while (last_n.empty()){
+	while (last_n.empty() && !std::cin.eof()){
 		std::cout << "Enter last name: ";
 		std::getline(std::cin, last_n);
 	}
-	while (nick_n.empty()){
+	while (nick_n.empty() && !std::cin.eof()){
 		std::cout << "Enter nickname: ";
 		std::getline(std::cin, nick_n);
 	}
-	while (phone_num.empty()){
+	while (phone_num.empty() && !std::cin.eof()){
 		std::cout << "Enter phone_number: ";
 		std::getline(std::cin, phone_num);
 	}
-	while (dark_secret.empty()){
+	while (dark_secret.empty() && !std::cin.eof()){
 		std::cout << "Enter darkest_secret: ";
 		std::getline(std::cin, dark_secret);
 	}
+
 	agenda.addContact(Contact(first_n, last_n, nick_n, phone_num, dark_secret));
-	if (std::cin.eof()==1) {
-		return ;
-	}
+
 	first_n.clear();
 	last_n.clear();
 	nick_n.clear();
