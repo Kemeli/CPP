3bc7b39c8f84477bc3c3b65339a7328223d6f440
Kemeli <kdnogueira791@gmail.com>
2023-10-06 10:39:56 -0300

    adds for loop to ex00

--- module_1/ex00/megaphone.cpp
+++ module_1/ex00/megaphone.cpp
@@ -6,21 +6,16 @@ int	main(int argc, char **argv)
 	int	j;
 
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
 	{
-		i = 1;
-		while (i < argc)
-		{
-			j = 0;
-			while(argv[i][j])
-			{
-				std::cout << (char)std::toupper(argv[i][j]);
-				j++;
-			}
-			std::cout << " ";
-			i++;
-		}
-		std::cout << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
 	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for(j = 0; argv[i][j]; j++)
+			std::cout << (char)std::toupper(argv[i][j]);
+		std::cout << " ";
+	}
+	std::cout << std::endl;
 }
