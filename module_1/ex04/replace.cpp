79ed24d0457870ddf04d59cdbc8bc85c54034e5e
Kemeli <kdnogueira791@gmail.com>
2023-10-18 09:41:03 -0300

    validates equal strings parameter

--- module_1/ex04/replace.cpp
+++ module_1/ex04/replace.cpp
@@ -6,14 +6,24 @@ std::string	replace(FileManipulation& obj, std::ifstream& inFile)
 	std::string	line;
 	size_t		position;
 	std::string	content;
+	std::string	replace;
+	std::string	replaced;
+	char		equal;
+
+	replaced = obj.get_be_replaced();
+	replace = obj.get_replace();
+
+	equal = 0;
+	if (replaced == replace)
+		equal = 1;
 
 	while(std::getline(inFile, line))
 	{
-		while (line.find(obj.get_be_replaced()) != std::string::npos)
+		while (!equal && line.find(replaced) != std::string::npos)
 		{
-			position = line.find(obj.get_be_replaced());
-			line.erase(position, obj.get_be_replaced().length());
-			line.insert(position, obj.get_replace());
+			position = line.find(replaced);
+			line.erase(position, replaced.length());
+			line.insert(position, replace);
 		}
 		content += line;
 		content.insert(content.length(), "\n");
